9815e3ee464753397739e699c4a838c37c0e57d9
jonathanowens2424 <jonathanlouisowens@gmail.com>
2026-03-23 20:52:13 -0500

    Submit Button properly updates your library with input data

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // 1. Create an array that stores books
-
+const form = document.querySelector("form");
 const myLibrary = [];
 
 // 2. Create a constructor that creates book object.
@@ -16,17 +16,15 @@ function addBookToLibrary(name, author, pages) {
   let book = new Book(name, author, pages);
   book.id = crypto.randomUUID();
   myLibrary.push(book);
+  renderBooks(myLibrary);
+  console.log(myLibrary);
 }
-
-addBookToLibrary("Bible", "God", 454);
-addBookToLibrary("Bible", "God", 454);
-addBookToLibrary("Bible", "God", 454);
-addBookToLibrary("Bible", "God", 454);
 
 // 4. Write a function that looks through the myLibrary Array and presents them into the HTML.
 
 function renderBooks(arr) {
-  const libraryHTML = document.getElementById("myLibrary");
+  let libraryHTML = document.getElementById("myLibrary");
+  libraryHTML.innerHTML = "";
   arr.forEach((book) => {
     libraryHTML.innerHTML += `
     <h2>${book.name}</h2>
@@ -35,8 +33,6 @@ function renderBooks(arr) {
     `;
   });
 }
-
-renderBooks(myLibrary);
 
 // 5. Create a "New Book" button that takes in inputs so the user can input a book into the array
 
@@ -48,6 +44,18 @@ newBookBtn.addEventListener("click", () => {
 const closeBtn = document.getElementById("closeBtn");
 closeBtn.addEventListener("click", () => {
   document.getElementById("newBookForm").close();
+  form.reset();
+});
+
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const bookTitle = document.getElementById("bookTitle").value;
+  const bookAuthor = document.getElementById("bookAuthor").value;
+  const bookPages = document.getElementById("bookPages").value;
+  addBookToLibrary(bookTitle, bookAuthor, bookPages);
+  document.getElementById("newBookForm").close();
+  form.reset();
 });
 
 //6. Add a button on each book's display to remove the book from the library. You will probably need to make a "RemoveBookFunction" that takes the item out of the array.
